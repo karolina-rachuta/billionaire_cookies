a906a9dd1cc37595dafa930f388eb1b31d155980
Karolina Rachuta <karolina.rachuta@gmail.com>
2025-02-03 13:20:33 +0400

    password update

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -46,6 +46,7 @@ const styles = {
         justifyContent: 'center',
         alignItems: 'center',
         height: '100vh',
+        width: '100%',
         background: 'rgba(10, 10, 10, 0.8)',
     },
     input: {
