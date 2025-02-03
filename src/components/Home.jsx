d04eacd9fcd81eca8bf9ed7251cfda80bbc0c937
Karolina Rachuta <karolina.rachuta@gmail.com>
2025-02-03 12:56:02 +0400

    suspense

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,33 @@
-import React from "react";
-import useContentful from "../useContenful";
-import Header from "./Home/Header";
+import React, { Suspense } from 'react';
+import useContentful from '../useContenful';
+import Header from './Home/Header';
 //const LazyHero = React.lazy(() => import('../components/Home/Hero'));
-import Hero from "./Home/Hero";
+import Hero from './Home/Hero';
 
 //import AboutUs from "./Home/AboutUs";
 const LazyAboutUs = React.lazy(() => import('../components/Home/AboutUs'));
 //import Order from "./Home/Order";
 const LazyOrder = React.lazy(() => import('../components/Home/Order'));
 //const LazyFlavours = React.lazy(() => import('../components/Home/Flavours'));
-import Flavours from "./Home/Flavours";
+import Flavours from './Home/Flavours';
 //import Footer from "./Home/Footer";
 const LazyFooter = React.lazy(() => import('../components/Home/Footer'));
 
 const Home = () => {
-  const [image]= useContentful();
+    const [image] = useContentful();
 
-  return (
-    <div>
-      <Header />
-      <Hero />
-      <Flavours image={image} />
-      <LazyAboutUs />
-      <LazyOrder />
-      <LazyFooter />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Hero />
+            <Flavours image={image} />
+            <Suspense fallback="Loading...">
+                <LazyAboutUs />
+                <LazyOrder />
+                <LazyFooter />
+            </Suspense>
+        </div>
+    );
 };
 
 export default Home;
